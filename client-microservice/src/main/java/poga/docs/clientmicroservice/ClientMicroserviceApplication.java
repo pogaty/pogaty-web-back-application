0a6e9874b5b1a527fdd0a69a93f8b851f426dc71
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-13 22:16:41 +0700

    added client microservice

---
+++ client-microservice/src/main/java/poga/docs/clientmicroservice/ClientMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package poga.docs.clientmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMicroserviceApplication.class, args);
+	}
+
+}
