d3f92c818663955ea98f778ce1b7a58396d46457
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-26 23:54:26 +0700

    update idea entity/ add attributes

--- client-microservice/src/main/java/poga/docs/clientmicroservice/models/Idea.java
+++ client-microservice/src/main/java/poga/docs/clientmicroservice/models/Idea.java
@@ -1,9 +1,9 @@
 package poga.docs.clientmicroservice.models;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,8 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
@@ -21,11 +23,13 @@ public class Idea {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("idea_id")
-    @Column(name = "idea_id")
-    private Long idea_id;
+    private Long id;
 
     @JsonProperty("ideaHeader")
     private String ideaHeader;
+
+    @JsonProperty("key")
+    private String key;
 
     @JsonProperty("board")
     private String board;
@@ -33,34 +37,45 @@ public class Idea {
     @JsonProperty("agreement")
     private int agreement;
 
-    
+    @JsonProperty("publicState")
+    private boolean publicState;
+
+    @JsonProperty("date")
+    @JsonFormat(pattern = "dd:MM:yyyy:HH:mm")
+    private LocalDateTime date;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private Client creator;
+
 
     // Releationship to entity M-->N
     @ManyToMany(cascade = CascadeType.PERSIST)
     @JoinTable(name = "idea_participants",
-                joinColumns = @JoinColumn(name="idea_id",referencedColumnName = "idea_id"),
-                inverseJoinColumns = @JoinColumn(name="participant_id" , referencedColumnName = "participant_id"))
-    private List<Participant> participant;
+                joinColumns = @JoinColumn(name="idea_id",referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name="participant_id" , referencedColumnName = "id"))
+    private List<Participant> participants;
 
     Idea(){
-        
+        this.date = LocalDateTime.now();
     }
 
-    public Idea(Long idea_id, String ideaHeader, String board, int agreement,
-            List<Participant> participants) {
-        this.idea_id = idea_id;
+    public Idea(Long id, String ideaHeader, String key, String board, int agreement, boolean publicState) {
+        super();
+        this.id = id;
         this.ideaHeader = ideaHeader;
+        this.key = key;
         this.board = board;
         this.agreement = agreement;
-        this.participant = participants;
+        this.publicState = publicState;
     }
 
     public Long getIdea_id() {
-        return idea_id;
+        return id;
     }
 
-    public void setIdea_id(Long idea_id) {
-        this.idea_id = idea_id;
+    public void setIdea_id(Long id) {
+        this.id = id;
     }
 
     public String getIdeaHeader() {
@@ -87,13 +102,43 @@ public class Idea {
         this.agreement = agreement;
     }
 
-    public List<Participant> getParticipant() {
-        return participant;
+    public List<Participant> getParticipants() {
+        return participants;
     }
 
-    public void setParticipant(List<Participant> participant) {
-        this.participant = participant;
+    public void setParticipant(List<Participant> participants) {
+        this.participants = participants;
     }
 
-    
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Client getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Client creator) {
+        this.creator = creator;
+    }
+
+    public boolean isPublicState() {
+        return publicState;
+    }
+
+    public void setPublicState(boolean publicState) {
+        this.publicState = publicState;
+    }
 }
--- client-microservice/src/main/java/poga/docs/clientmicroservice/models/IdeaDTO.java
+++ client-microservice/src/main/java/poga/docs/clientmicroservice/models/IdeaDTO.java
@@ -1,13 +1,19 @@
 package poga.docs.clientmicroservice.models;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class IdeaDTO {
     @JsonProperty("idea_id")
-    private Long idea_id;
+    private Long id;
 
     @JsonProperty("ideaHeader")
     private String ideaHeader;
+
+    @JsonProperty("key")
+    private String key;
 
     @JsonProperty("board")
     private String board;
@@ -15,12 +21,18 @@ public class IdeaDTO {
     @JsonProperty("agreement")
     private int agreement;
 
+    @JsonProperty("publicState")
+    private boolean publicState;
+
+    @JsonFormat(pattern = "dd:MM:yyyy:HH:mm")
+    private LocalDateTime date;
+
     public Long getIdea_id() {
-        return idea_id;
+        return id;
     }
 
-    public void setIdea_id(Long idea_id) {
-        this.idea_id = idea_id;
+    public void setIdea_id(Long id) {
+        this.id = id;
     }
 
     public String getIdeaHeader() {
@@ -45,6 +57,30 @@ public class IdeaDTO {
 
     public void setAgreement(int agreement) {
         this.agreement = agreement;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public boolean isPublicState() {
+        return publicState;
+    }
+
+    public void setPublicState(boolean publicState) {
+        this.publicState = publicState;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 
     
