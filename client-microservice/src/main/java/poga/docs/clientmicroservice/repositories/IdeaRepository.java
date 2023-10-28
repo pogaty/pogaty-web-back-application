c1c97dd7133ac59d0c013768ce84e4f9dfb5f032
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-28 10:12:31 +0700

    update idea/ add findById

--- client-microservice/src/main/java/poga/docs/clientmicroservice/repositories/IdeaRepository.java
+++ client-microservice/src/main/java/poga/docs/clientmicroservice/repositories/IdeaRepository.java
@@ -1,6 +1,7 @@
 package poga.docs.clientmicroservice.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,4 +11,5 @@ public interface IdeaRepository extends CrudRepository<Idea, Long>{
     public List<Idea> findAll();
     public Idea findByIdeaHeader(String ideaHeader);
     public List<Idea> findByIdeaHeaderStartingWith(String prefix);
+    public Optional<Idea> findById(Long id);
 }
--- client-microservice/src/main/java/poga/docs/clientmicroservice/services/IdeaService.java
+++ client-microservice/src/main/java/poga/docs/clientmicroservice/services/IdeaService.java
@@ -1,6 +1,7 @@
 package poga.docs.clientmicroservice.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,9 @@ public class IdeaService {
 
     public List<Idea> findByHeaderIdeaStartingWith(String prefix) {
         return this.ideaRepository.findByIdeaHeaderStartingWith(prefix);
+    }
+
+    public Optional<Idea> findByIdeaId(Long id) {
+        return this.ideaRepository.findById(id);
     }
 }
